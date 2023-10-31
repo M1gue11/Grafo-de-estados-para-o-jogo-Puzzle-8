42e874068af369d1188db673005c826b053ab373
brunomikmenta <bruno.mikmenta@gmail.com>
2023-10-30 22:01:18 -0300

    prints pra parte 1 + gitignore

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # Miguel Angelus Araruna de Aquino
-# Bruno Mikmenta
+# Bruno Miksucas Pimenta
 
 from models import Grafo, No
 import itertools
@@ -9,6 +9,11 @@ from tqdm import tqdm
 
 pecas = (1,2,3,4,5,6,7,8,0)
 permutacoes_tuplas = list(itertools.permutations(pecas))
+total_nos = len(permutacoes_tuplas)
+total_arestas = 0
+print('Há', total_nos, 'nós no grafo do espaço de estados')
+
+
 grafo = Grafo()
 for tupla in tqdm(permutacoes_tuplas):
     no = grafo.get_no_by_cfg(tupla)
@@ -17,6 +22,7 @@ for tupla in tqdm(permutacoes_tuplas):
 
     grafo.adicionar_no(no)
     movimentos = no.movimentos_validos()
+    total_arestas += len(movimentos)
     # print(no.to_string())
 
     for movimento_idx in movimentos:
@@ -29,6 +35,7 @@ for tupla in tqdm(permutacoes_tuplas):
         grafo.adicionar_no(novo_no)
         grafo.adicionar_aresta(no, novo_no)
 
+print('Há', total_arestas, 'arestas no grafo do espaço de estados')
 # print(grafo.to_string())
 print(grafo.grafo.__len__())
 
